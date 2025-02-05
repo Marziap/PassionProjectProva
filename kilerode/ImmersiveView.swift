a1ac7c2539cc7687a497314d81a412912b29493e
Marziap <marziapirozzi2002@gmail.com>
2025-02-05 10:28:51 +0100

    disable meteorite after animation

--- kilerode/ImmersiveView.swift
+++ kilerode/ImmersiveView.swift
@@ -20,7 +20,10 @@ struct ImmersiveView: View {
     @State var swordModel = Entity()
     @State var leverModel = Entity()
     @State var cameraModel = Entity()
-    @State private var session: SpatialTrackingSession?
+    @State var session: SpatialTrackingSession?
+    @State var subscription: EventSubscription?
+    @State var thisContent: RealityViewContent?
+    @State var swordAnchohor: Bool = true
     
     
     
@@ -28,53 +31,86 @@ struct ImmersiveView: View {
         
         RealityView { content in
             
+            self.thisContent = content
+            
             // Add the initial RealityKit content
             if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
                 content.add(immersiveContentEntity)
                 
                 immersiveContentEntity.position = [-0.1, 0.05, -0.5] // offset for the user to osit at the armchair
-                
-                let session = SpatialTrackingSession()
-                        let configuration = SpatialTrackingSession.Configuration(tracking: [.hand])
-                        _ = await session.run(configuration)
-                        self.session = session
-                        
-                        //Setup an anchor at the user's left palm.
-                let handAnchor = AnchorEntity(.hand(.right, location: .palm), trackingMode: .continuous)
-
-                        //Add the Gauntlet scene that was set up in Reality Composer Pro.
-                        if let gauntletEntity = try? await Entity(named: "Untitled Scene", in: realityKitContentBundle) {
-                           
-                            if let gauntlet = gauntletEntity.findEntity(named: "Gauntlet"){
-                                
-                                
-                                //Child the gauntlet scene to the handAnchor.
-                                handAnchor.addChild(gauntlet)
-                                
-                                // Add the handAnchor to the RealityView scene.
-                                content.add(handAnchor)
-                                
-                            }
-                           
-                           
-                        }
+                if let swordScene = try? await Entity(named: "Sword Scene", in: realityKitContentBundle) {
+                    
+                    if let sword = swordScene.findEntity(named: "Sword"){
+                        self.swordModel = sword
+                    }
+                }
                 
                 if let meteor = immersiveContentEntity.findEntity(named: "Meteor"),
-                   let sword = immersiveContentEntity.findEntity(named: "Sword"),
+                   //                   let sword = immersiveContentEntity.findEntity(named: "Sword"),
                    let lever = immersiveContentEntity.findEntity(named: "Lever"),
                    let camera = immersiveContentEntity.findEntity(named: "Camera")
                 {
                     self.meteorModel = meteor
-                    self.swordModel = sword
+                    //                    self.swordModel = sword
                     self.leverModel = lever
                     self.cameraModel = camera
                 }
                 
-                swordModel.components.set(HoverEffectComponent(.spotlight(
-                    HoverEffectComponent.SpotlightHoverEffectStyle(
-                        color: .yellow, strength: 2.0
-                    )
-                )))
+                
+                let session = SpatialTrackingSession()
+                let configuration = SpatialTrackingSession.Configuration(tracking: [.hand])
+                _ = await session.run(configuration)
+                self.session = session
+                
+                if(swordAnchohor == false){
+                    //Setup an anchor at the user's left palm.
+                    let handAnchor = AnchorEntity(.hand(.right, location: .palm), trackingMode: .continuous)
+                    
+                    //Add the Gauntlet scene that was set up in Reality Composer Pro.
+                    if let gauntletEntity = try? await Entity(named: "Untitled Scene", in: realityKitContentBundle) {
+                        
+                        if let gauntlet = gauntletEntity.findEntity(named: "Gauntlet"){
+                            
+                            
+                            //Child the gauntlet scene to the handAnchor.
+                            handAnchor.addChild(gauntlet)
+                            
+                            // Add the handAnchor to the RealityView scene.
+                            content.add(handAnchor)
+                            
+                        }
+                        
+                        
+                    }
+                }else{
+                    let handAnchor = AnchorEntity(.hand(.right, location: .palm), trackingMode: .continuous)
+                    
+                    //Add the Gauntlet scene that was set up in Reality Composer Pro.
+                    if let swordScene = try? await Entity(named: "Sword Scene", in: realityKitContentBundle) {
+                        
+                        if let sword = swordScene.findEntity(named: "Sword"){
+                            
+                            //Child the gauntlet scene to the handAnchor.
+                            handAnchor.addChild(sword)
+                            
+                            // Add the handAnchor to the RealityView scene.
+                            content.add(handAnchor)
+                            
+                        }
+                        
+                        
+                    }
+                }
+                
+                
+                
+                
+                
+                //                swordModel.components.set(HoverEffectComponent(.spotlight(
+                //                    HoverEffectComponent.SpotlightHoverEffectStyle(
+                //                        color: .yellow, strength: 2.0
+                //                    )
+                //                )))
                 leverModel.components.set(HoverEffectComponent(.spotlight(
                     HoverEffectComponent.SpotlightHoverEffectStyle(
                         color: .yellow, strength: 2.0
@@ -90,6 +126,24 @@ struct ImmersiveView: View {
                         color: .yellow, strength: 2.0
                     )
                 )))
+                
+            }
+            
+            subscription = content.subscribe(to: CollisionEvents.Began.self, on: meteorModel) { collisionEvent in
+                
+                if (collisionEvent.entityB.name == "Sword") {
+                    print("Collision")
+                    let animation = meteorModel.availableAnimations[0]
+                    let duration = animation.definition.duration  // Get the animation duration
+                    meteorModel.components.remove(CollisionComponent.self)
+                    
+                    meteorModel.playAnimation(animation)
+                    // Delay disabling the entity until after the animation ends
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+                        meteorModel.isEnabled = false
+                    }
+                }
+
             }
             
             //            let material = PhysicsMaterialResource.generate(friction: 0.8, restitution: 0.0)
@@ -109,12 +163,13 @@ struct ImmersiveView: View {
                 //                cooz = value.location3D.z
             }))
         
-            .gesture(SpatialTapGesture().targetedToEntity(where: .has(MeteorComponent.self)).onEnded({ value in
-                print("animate")
-                let animation = value.entity.availableAnimations[0]
-                value.entity.playAnimation(animation)
-                
-            }))
+        //            .gesture(SpatialTapGesture().targetedToEntity(where: .has(MeteorComponent.self)).onEnded({ value in
+        //                print("animate")
+        //                let animation = value.entity.availableAnimations[0]
+        //                value.entity.playAnimation(animation)
+        //
+        //            }))
+        
             .gesture(
                 SpatialTapGesture()
                     .targetedToEntity(where: .has(TimelineComponent.self))
@@ -124,12 +179,6 @@ struct ImmersiveView: View {
                         }
                     }
             )
-            .upperLimbVisibility(.hidden)
+        //            .upperLimbVisibility(.hidden)
     }
-}
-
-
-#Preview(immersionStyle: .mixed) {
-    ImmersiveView()
-        .environment(AppModel())
 }
